1e471e02311b6a82b4feb393c8ba16716ea0f337
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2022-03-06 22:27:02 +0530

    added categories

--- src/components/MainExpense.jsx
+++ src/components/MainExpense.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { expenseCategories, incomeCategories } from '../constants/categories';
 import { useStateValue } from '../state/StateProvider';
 import ListExpense from './ListExpense';
 import './mainExpense.css';
@@ -8,7 +9,7 @@ const MainExpense = () => {
     const [{ transactions }, dispatch] = useStateValue();
 
     const [type, setType] = useState("income")
-    const [category, setCategory] = useState("Category")
+    const [category, setCategory] = useState("")
     const [amount, setAmount] = useState(0)
     const [date, setDate] = useState("")
 
@@ -30,12 +31,6 @@ const MainExpense = () => {
         setType("income")
     }
 
-    // const handleChange = (e) => {
-    //     const name = e.target.name;
-    //     const value = e.target.value;
-    //     setData(values => ({ ...values, [name]: value }))
-    // }
-
     const handleSelectType = (e) => {
         setType(e.target.value)
     }
@@ -51,6 +46,7 @@ const MainExpense = () => {
         setDate(e.target.value)
     }
 
+    const selectedCategories = type === 'income' ? incomeCategories : expenseCategories
 
     return (
         <div className='MainExpense'>
@@ -78,24 +74,22 @@ const MainExpense = () => {
                                         </select>
                                     </div>
                                     <div className="expense-category">
-                                        <select id="category" value={category} onChange={handleSelectCategory}>
-                                            <option defaultValue='Category'>Category</option>
-                                            <option value="shopping">Shopping</option>
-                                            <option value="fees">Fees</option>
-                                            <option value="friend">Friend</option>
-                                            <option value="travelling">Travelling</option>
+                                        <select id="category" value={category} onChange={handleSelectCategory} required>
+                                            {selectedCategories.map((category) => (
+                                                <option key={category.type} value={category.type}>{category.type}</option>
+                                            ))}
                                         </select>
                                     </div>
                                     <div className='amount'>
                                         <input type="text" name='amount' placeholder='Amount' value={amount}
-                                            onChange={handleAmount} />
+                                            onChange={handleAmount} required />
                                     </div>
                                     <div className='date'>
-                                        <input type="date" name='date' value={date} onChange={handleDate} />
+                                        <input type="date" name='date' value={date} onChange={handleDate} required />
                                     </div>
                                 </div>
                             </form>
-                            <button className='btn' onClick={addTransaction}>Add</button>
+                            <button className='btn' onClick={addTransaction} disabled={amount === 0}>Add</button>
                         </div>
                     </div>
                     <div className="expense-data">
